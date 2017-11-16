4d1aea81fd02b1831f33d2324a0782e554e20186
Zidaan Dutta <zdutta@purdue.edu>
2017-11-15 19:33:34 -0800

    added some basic functionality

--- quickopen.py
+++ quickopen.py
@@ -1,9 +1,36 @@
 import webbrowser
 
-#webbrowser.get('firefox').open_new_tab('http://www.google.com')
-websites = open("websites.txt","r")
-#print websites.readline()
-for line in websites.readlines():
-	webbrowser.get('firefox').open_new_tab(line)
-task = raw_input()
+# #webbrowser.get('firefox').open_new_tab('http://www.google.com')
+# websites = open("websites.txt","r")
+# #print websites.readline()
+# for line in websites.readlines():
+# 	webbrowser.get('firefox').open_new_tab(line)
+# task = raw_input()
+
+websites={}
+
+def addGroup(groupName):
+	websites[groupName] = []
+
+def addToGroup(groupName,link):
+	websites[groupName].append(link)
+
+def open(groupName):
+	for site in websites[groupName]:
+		webbrowser.get('firefox').open_new_tab(site)
+
+
+while(1):
+	command = raw_input()
+	if(command[0:9] == "add group"):
+		addGroup(command[10:])
+	elif(command[0:6] == "add to"):
+		group = command[7:]
+		newLink = raw_input()
+		addToGroup(group,newLink)
+	elif(command[0:4] == "open"):
+		group = command[5:]
+		open(group)
+	elif(command == "end"):
+		break
 
