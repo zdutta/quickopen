a0bb0a3577aa7f0c0718f473241558e10c16f810
Zidaan Dutta <zdutta@purdue.edu>
2017-11-27 12:05:15 -0800

    added a few prompts

--- quickopen.py
+++ quickopen.py
@@ -1,5 +1,6 @@
 import webbrowser
 import sys
+import os
 # #webbrowser.get('firefox').open_new_tab('http://www.google.com')
 # websites = open("websites.txt","r")
 # #print websites.readline()
@@ -8,7 +9,7 @@ import sys
 # task = raw_input()
 
 
-websites={}
+websites={} #dictionary
 
 def addGroup(groupName):
 	# websites[groupName] = []
@@ -30,21 +31,32 @@ def openGroup(groupName):
 	 	webbrowser.get('firefox').open_new_tab(line)
 	 file.close()
 
-
+def removeGroup(groupName):
+	os.remove(("websites/{}.txt").format(groupName))
 
 
 while(1):
+	print "Enter a command:"
 	command = raw_input()
 	if(command[0:9] == "add group"):
 		addGroup(command[10:])
+		print command[10:] + " added"
 	elif(command[0:6] == "add to"):
 		group = command[7:]
+		print "Enter link to add to " + group
 		newLink = raw_input()
 		addToGroup(group,newLink)
+		print newLink + " added to " + group
 	elif(command[0:4] == "open"):
 		group = command[5:]
 		openGroup(group)
+		print group + " opened"
+	elif( command[:12] == "remove group"):
+		group = command[13:]
+		removeGroup(group)
+		print group + " removed"
 	elif(command == "end"):
+		print "application ended"
 		break
 	else:
 		print "Invalid command"
